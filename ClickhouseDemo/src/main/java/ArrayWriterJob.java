e61b82352bbe11496b3fd0c210bcdcaa2855c292
凌涛 <iamlt_2015@163.com>
2021-04-19 20:15:16 +0800

    add demo

--- ClickhouseDemo/src/main/java/ArrayWriterJob.java
+++ ClickhouseDemo/src/main/java/ArrayWriterJob.java
@@ -12,7 +12,7 @@ import java.util.Random;
 
 public class ArrayWriterJob {
     public static void main(String[] args) {
-        String clickhouseUrl = "jdbc:clickhouse://macmini:8123";
+        String clickhouseUrl = "jdbc:clickhouse://localhost:8123";
         ClickHouseProperties clickHouseProperties = new ClickHouseProperties();
         clickHouseProperties.setUser("default");
         clickHouseProperties.setDatabase("test");
@@ -29,22 +29,25 @@ public class ArrayWriterJob {
 
         try (ClickHouseConnection connection = dataSource.getConnection()) {
             ClickHouseStatement sth = connection.createStatement();
-            sth.write().send("INSERT INTO test.arra_test", new ClickHouseStreamCallback() {
+            sth.write().send("INSERT INTO test.array_test", new ClickHouseStreamCallback() {
                 @Override
                 public void writeTo(ClickHouseRowBinaryStream stream) throws IOException {
-                    stream.writeString(DataGenerator.generateRandomString(4));
-                    stream.writeInt32(rd.nextInt(120));
-                    String[] values = new String[30];
+                    for (int rowCount = 1; rowCount <= 200000; rowCount++) {
+                        stream.writeString(DataGenerator.generateRandomString(4));
+                        stream.writeInt32(rd.nextInt(120));
+                        String[] values = new String[30];
 
-                    for(int index = 0; index < 30; index++) {
-                        values[index] = DataGenerator.generateRandomString(30);
+                        for (int index = 0; index < 30; index++) {
+                            values[index] = DataGenerator.generateRandomString(30);
+                        }
+
+                        stream.writeStringArray(keys);
+                        stream.writeStringArray(values);
+                        stream.writeString(DataGenerator.generateRandomCityNameFromChina());
                     }
-
-                    stream.writeStringArray(keys);
-                    stream.writeStringArray(values);
-                    stream.writeString(DataGenerator.generateRandomCityNameFromChina());
                 }
             }, ClickHouseFormat.RowBinary);
+
         } catch (Exception e) {
             System.out.println("Error: " + e.getMessage());
             System.exit(-1);
